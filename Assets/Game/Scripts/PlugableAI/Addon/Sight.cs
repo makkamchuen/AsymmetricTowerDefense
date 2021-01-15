f175b075bc4f2188199ae8239abf2db798d1acb8
davidlam <yuensengd@gmail.com>
2021-01-14 18:32:41 -0800

    feat: added finite state machine for ai, modified .gitignore to ignore files produced mac and rider

---
+++ Assets/Game/Scripts/PlugableAI/Addon/Sight.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Addon/Sight"), Serializable]
+public class Sight: ScriptableObject
+{
+  [SerializeField] private float radius;
+  [TagSelector, SerializeField] private string targetTag;
+
+  public bool SearchTarget(StateController controller)
+  {
+    bool found = false;
+    Collider[] colliders = Physics.OverlapSphere(controller.transform.position, radius);
+    foreach (Collider collider in colliders) // may be move logic out to Decision?
+    {
+      if (collider.CompareTag(targetTag) 
+          && Compare(collider.gameObject, controller.gameObject, controller)
+          )
+      {
+        controller.target = collider.gameObject;
+        found = true;
+        break;
+      }
+    }
+    return found;
+  }
+
+  private bool Compare(GameObject candidate, GameObject original, StateController controller)
+  {
+    return true;
+  }
+}
---
+++ Assets/Game/Scripts/PlugableAI/Addon/Stats.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Addon/Stats"), Serializable]
+public class Stats : ScriptableObject
+{
+
+  public Sight[] sights;
+  public PatrolPattern patrolPattern;
+}
