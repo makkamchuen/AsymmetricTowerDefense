7b53c478242993f1b6ce93c54e7e19ddaf736418
chacobo <50167935+chacobo@users.noreply.github.com>
2021-01-14 21:42:43 -0800

    Updated prefab & inheritance

---
+++ Assets/Game/Scripts/Dev/DrawGizmo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawGizmo : MonoBehaviour
+{
+    // [SerializeField] private Mesh mesh;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireMesh(GetComponent<MeshFilter>().sharedMesh, transform.position, Quaternion.identity, transform.lossyScale);
+    }
+}
