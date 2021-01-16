06166aed11b819671e628deaed2e465436fe5817
chacobo <50167935+chacobo@users.noreply.github.com>
2021-01-16 00:25:28 -0800

    Added Gizmo dev tool

---
+++ Assets/Game/Scripts/Dev/GizmoMeshFilter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmoMeshFilter : MonoBehaviour
+{
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireMesh(GetComponent<MeshFilter>().sharedMesh, transform.position, Quaternion.identity, transform.lossyScale);
+    }
+}
