bf811983388ec145404524e5af215b3acd1913f0
davidlam <yuensengd@gmail.com>
2021-01-29 12:58:04 -0800

    feat: added basic summon skill

--- Assets/Game/Scripts/Skill/SummonSkillData.cs
+++ Assets/Game/Scripts/Skill/SummonSkillData.cs
@@ -1,12 +1,16 @@
-﻿using UnityEngine;
+﻿using Game.Scripts;
+using UnityEngine;
 
 [CreateAssetMenu (menuName = "Skill/Summon")]
 public class SummonSkillData : SkillData
 {
-    [SerializeField] private Actor actor;
+    [SerializeField] private GameObject _gameObject;
 
     public override void Cast(Actor user, Vector3 destination)
     {
+        GameObject gameObject = PoolManager.Instantiate(_gameObject);
+        gameObject.transform.position = user.transform.position;
+        gameObject.transform.LookAt(destination);
     }
 
     public override bool InRange(Actor user, Actor targetActor)
