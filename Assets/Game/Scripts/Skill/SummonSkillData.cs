823c1fc92b7ac95725756a11019a73ec01e5df8d
Howie Leung <yuenhol@yahoo.com>
2021-02-11 21:44:32 -0800

    It seems spawn only works on the first time you run..  But second time won't work because the number of cast count is saved into Scriptable object.   Now move it out of number of cast out of skill data.

--- Assets/Game/Scripts/Skill/SummonSkillData.cs
+++ Assets/Game/Scripts/Skill/SummonSkillData.cs
@@ -1,23 +1,27 @@
 ﻿using Game.Scripts;
+using Game.Scripts.Skill;
 using UnityEngine;
 
 [CreateAssetMenu (menuName = "Skill/Summon")]
-public class SummonSkillData : SkillData
+public class SummonSkillData : SkillData, IMaxCastApply
 {
     [SerializeField] private GameObject _gameObject;
-    [SerializeField] private int spawnSize;
+    [SerializeField] private int maxNumOfCast;
     [SerializeField] private int reachableDistance;
-    private int spawnnedCount = 0;
 
     public override void Cast(Actor user, Vector3 destination)
     {
         GameObject gameObject = PoolManager.Spawn(_gameObject, destination);
         gameObject.transform.LookAt(destination);
-        spawnnedCount++;
     }
 
     public override bool CanApply(Actor user, Actor targetActor)
     {
-        return spawnnedCount >= spawnSize && Vector3.Distance(user.transform.position, targetActor.transform.position) <= reachableDistance - 1;
+        return Vector3.Distance(user.transform.position, targetActor.transform.position) <= reachableDistance - 1;
+    }
+
+    public int GetMaxNumberOfCast()
+    {
+        return maxNumOfCast;
     }
 }
