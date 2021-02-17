08e34581cfa788c9f5e446ed048ed2d6da4d02f8
Howie Leung <yuenhol@yahoo.com>
2021-02-16 20:29:14 -0800

    When flee, face to the direction it is running. Flee with min and max distance using the radius of the character instead of target. After flee, run back to the same location.

--- Assets/Game/Scripts/StateMachine/State/FleeState.cs
+++ Assets/Game/Scripts/StateMachine/State/FleeState.cs
@@ -1,16 +1,23 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 [CreateAssetMenu (menuName = "StateMachine/State/Flee State")]
 public class FleeState: State
 {
+    private Actor _target;
+    private Vector3 _prevPosition;
+    
     public override void OnEnter(StateController controller)
     {
         base.OnEnter(controller);
+        _target = controller.target.GetTargetActor();
+        _prevPosition = controller.transform.position;
         if (controller.target is Minion minion)
         {
-            var pos = RandomPoint(minion.transform.position, 2f);
-            minion.GetMover().GetNavMeshAgent().SetDestination(pos);
+            var pos = RandomPoint(minion.transform.position, 1.5f, 4f);
+            minion.GetMover().MoveTo(pos);
         }
     }
 
@@ -19,22 +26,17 @@ public class FleeState: State
         base.OnExit(controller);
         if (controller.target is Minion minion)
         {
-            if (minion.GetTargetActor() == minion)
-                return;
-            Vector3 dir = minion.transform.position - minion.GetTargetActor().transform.position;
-            dir.y = 0;//This allows the object to only rotate on its y axis
-            Quaternion rot = Quaternion.LookRotation(dir);
-            minion.transform.rotation = Quaternion.Lerp(minion.transform.rotation, rot, 0.01f);
+            minion.GetMover().MoveTo(_prevPosition);
         }
     }
 
-    Vector3 RandomPoint(Vector3 center, float range)
+    Vector3 RandomPoint(Vector3 center, float minDist, float maxDist)
     {
-        for (int i = 0; i < 30; i++)
+        for (int i = 0; i < 300; i++)
         {
-            Vector3 randomPoint = center + Random.insideUnitSphere * range;
+            Vector3 randomPoint = center + Random.insideUnitSphere * Random.Range(minDist, maxDist);
             NavMeshHit hit;
-            if (NavMesh.SamplePosition(randomPoint, out hit, 3f, NavMesh.AllAreas))
+            if (NavMesh.SamplePosition(randomPoint, out hit, 0.01f, NavMesh.AllAreas))
             {
                 return hit.position;
             }
