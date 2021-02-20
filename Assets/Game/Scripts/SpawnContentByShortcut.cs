4c3fa8c6c2e1ccd70770e859f3875bea735fb70f
Howie Leung <yuenhol@yahoo.com>
2021-02-19 22:21:46 -0800

    Spawn Bear by b key.  Spawn Ninja by n key.  Spawn Thanatos by t

---
+++ Assets/Game/Scripts/SpawnContentByShortcut.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+namespace Game.Scripts
+{
+    [Serializable]
+    public class SpawnContentByShortcut
+    {
+        [SerializeField] private Sprite sprite;
+        [SerializeField][FMODUnity.EventRef] private string spawnSound;
+        [SerializeField] private string shortcut;
+        [SerializeField] private GameObject prefab;
+        private Texture2D cursor;
+
+        public Texture2D Cursor
+        {
+            get
+            {
+                if (!cursor)
+                    cursor = sprite.texture;
+                return cursor;
+            }
+        }
+
+        public string SpawnSound => spawnSound;
+        public string Shortcut => shortcut;
+        public GameObject Prefab => prefab;
+
+    }
+}
