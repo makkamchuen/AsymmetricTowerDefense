02b2748a53a9960576d928a131170db252cf9135
chacobo <50167935+chacobo@users.noreply.github.com>
2021-04-09 01:23:40 -0700

    test

--- Assets/Game/Scripts/MapGeneration/MapManager.cs
+++ Assets/Game/Scripts/MapGeneration/MapManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class MapManager : MonoBehaviour 
+public class MapManager : MonoBehaviour
 {
 
     public int width; // n
@@ -23,7 +23,7 @@ public class MapManager : MonoBehaviour
     [SerializeField] private int _roadBlockCol;
     [SerializeField] private int _roadBlockHeight;
 
-    [HideInInspector] public int[,] map;
+    [HideInInspector] public int[, ] map;
     private MeshGenerator meshGen;
     private RandomSprite _randomSprite;
     [SerializeField] private float _colliderHeight;
@@ -78,23 +78,23 @@ public class MapManager : MonoBehaviour
             Destroy(_previousMap.gameObject);
         }
     }
-    
+
     public void SetPreviousMap(MapManager previousMap)
     {
         _previousMap = previousMap;
         mapNumber = previousMap.GetMapNumber() + 1;
         _mapSetted = true;
     }
-    
+
     public void PlaceRoadBlock()
     {
         if (!_roadBlockPlaced)
         {
             _roadBlockPlaced = true;
             GameObject newRoadBlock = Instantiate(
-                _roadBlock, 
-                new Vector3((-width/2 + _roadBlockCol) * unit + transform.position.x, 0, (-height/2 + mapGenerator.GetStartPoint().tileY) * unit + transform.position.z), 
-                Quaternion.identity, 
+                _roadBlock,
+                new Vector3((-width / 2 + _roadBlockCol) * unit + transform.position.x, 0, (-height / 2 + mapGenerator.GetStartPoint().tileY) * unit + transform.position.z),
+                Quaternion.identity,
                 transform
             );
         }
@@ -102,7 +102,7 @@ public class MapManager : MonoBehaviour
 
     private void InitGameObjects()
     {
-        InitDestroyCollider();
+        // InitDestroyCollider();
         InitNextMapCollider();
         InitRoadBlock();
     }
@@ -132,21 +132,21 @@ public class MapManager : MonoBehaviour
     private void PlaceColliders()
     {
         GameObject newDestroyCollider = Instantiate(
-            _destroyCollider, 
-            new Vector3((-width/2 + _destroyColliderCol) * unit + transform.position.x, 0,0), 
-            Quaternion.identity, 
+            _destroyCollider,
+            new Vector3((-width / 2 + _destroyColliderCol) * unit + transform.position.x, 0, 0),
+            Quaternion.identity,
             transform
         );
-        
+
         GameObject newNextMapCollider = Instantiate(
-            _nextMapCollider, 
-            new Vector3((-width/2 + _nextMapColliderCol) * unit + transform.position.x, 0,0), 
-            Quaternion.identity, 
+            _nextMapCollider,
+            new Vector3((-width / 2 + _nextMapColliderCol) * unit + transform.position.x, 0, 0),
+            Quaternion.identity,
             transform
         );
     }
 
-    void Update() 
+    void Update()
     {
         if (!_mapBuilt && (_mapSetted || mapNumber == 0))
         {
@@ -158,7 +158,7 @@ public class MapManager : MonoBehaviour
     public void GenerateMap()
     {
         _planeBuilder.BuildPlane();
-        if (useRandomSeed) 
+        if (useRandomSeed)
         {
             seed = System.DateTime.Now.ToString();
         }
@@ -178,14 +178,14 @@ public class MapManager : MonoBehaviour
         PlaceColliders();
     }
 
-    private int[,] GetInversedMap()
+    private int[, ] GetInversedMap()
     {
-        int[,] inversedMap = new int[width, height];
-        for (int x = 0; x < width; x ++) 
+        int[, ] inversedMap = new int[width, height];
+        for (int x = 0; x < width; x++)
         {
-            for (int y = 0; y < height; y ++) 
+            for (int y = 0; y < height; y++)
             {
-                inversedMap[x, y] = map[x, y] == 1? 0: 1;
+                inversedMap[x, y] = map[x, y] == 1 ? 0 : 1;
             }
         }
         return inversedMap;
@@ -234,11 +234,11 @@ public class MapManager : MonoBehaviour
 
     void SmoothMap() // O(nm)
     {
-        for (int x = 0; x < width; x += 1) 
+        for (int x = 0; x < width; x += 1)
         {
-            for (int y = 0; y < height; y += 1) 
+            for (int y = 0; y < height; y += 1)
             {
-                int neighbourWallTiles = GetSurroundingWallCount(x,y);
+                int neighbourWallTiles = GetSurroundingWallCount(x, y);
                 if (neighbourWallTiles > 4)
                 {
                     map[x, y] = 1;
@@ -252,82 +252,83 @@ public class MapManager : MonoBehaviour
     }
 
     int GetSurroundingWallCount(int gridX, int gridY) // O(1)
-    { 
+    {
         int wallCount = 0;
-        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX += 1) 
+        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX += 1)
         {
-            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY += 1) 
+            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY += 1)
             {
-                if (IsInMapRange(neighbourX, neighbourY)) 
+                if (IsInMapRange(neighbourX, neighbourY))
                 {
-                    if (neighbourX != gridX || neighbourY != gridY) 
+                    if (neighbourX != gridX || neighbourY != gridY)
                     {
-                        wallCount += map[neighbourX,neighbourY];
+                        wallCount += map[neighbourX, neighbourY];
                     }
                 }
-                else 
+                else
                 {
-                    wallCount ++;
+                    wallCount++;
                 }
             }
         }
         return wallCount;
     }
-    
-    bool IsInMapRange(int x, int y) 
+
+    bool IsInMapRange(int x, int y)
     {
         return x >= 0 && x < width && y >= 0 && y < height;
     }
-    
-    public void CreatePassage(Coordinate tileA, Coordinate tileB, int val = 0) 
+
+    public void CreatePassage(Coordinate tileA, Coordinate tileB, int val = 0)
     {
-        List<Coordinate> line = GetLine (tileA, tileB);
-        foreach (Coordinate c in line) 
+        List<Coordinate> line = GetLine(tileA, tileB);
+        foreach (Coordinate c in line)
         {
             FillPoint(c, val);
         }
     }
-    
-    private List<Coordinate> GetLine(Coordinate from, Coordinate to) 
+
+    private List<Coordinate> GetLine(Coordinate from, Coordinate to)
     {
-        List<Coordinate> line = new List<Coordinate> ();
+        List<Coordinate> line = new List<Coordinate>();
         line.Add(to);
         int x = from.tileX;
         int y = from.tileY;
         int dx = to.tileX - from.tileX;
         int dy = to.tileY - from.tileY;
         bool inverted = false;
-        int step = Math.Sign (dx);
-        int gradientStep = Math.Sign (dy);
-        int longest = Mathf.Abs (dx);
-        int shortest = Mathf.Abs (dy);
-        if (longest < shortest) {
+        int step = Math.Sign(dx);
+        int gradientStep = Math.Sign(dy);
+        int longest = Mathf.Abs(dx);
+        int shortest = Mathf.Abs(dy);
+        if (longest < shortest)
+        {
             inverted = true;
             longest = Mathf.Abs(dy);
             shortest = Mathf.Abs(dx);
-            step = Math.Sign (dy);
-            gradientStep = Math.Sign (dx);
+            step = Math.Sign(dy);
+            gradientStep = Math.Sign(dx);
         }
         int gradientAccumulation = longest / 2;
-        for (int i = 0; i < longest; i += 1) 
+        for (int i = 0; i < longest; i += 1)
         {
-            line.Add(new Coordinate(x,y));
-            if (inverted) 
+            line.Add(new Coordinate(x, y));
+            if (inverted)
             {
                 y += step;
             }
-            else 
+            else
             {
                 x += step;
             }
             gradientAccumulation += shortest;
-            if (gradientAccumulation >= longest) 
+            if (gradientAccumulation >= longest)
             {
-                if (inverted) 
+                if (inverted)
                 {
                     x += gradientStep;
                 }
-                else 
+                else
                 {
                     y += gradientStep;
                 }
@@ -342,36 +343,36 @@ public class MapManager : MonoBehaviour
         DrawCircle(c, val);
     }
 
-    private void DrawCircle(Coordinate c, int val) 
+    private void DrawCircle(Coordinate c, int val)
     {
-        for (int x = -pathRadius; x <= pathRadius; x += 1) 
+        for (int x = -pathRadius; x <= pathRadius; x += 1)
         {
-            for (int y = -pathRadius; y <= pathRadius; y += 1) 
+            for (int y = -pathRadius; y <= pathRadius; y += 1)
             {
-                if (x*x + y*y <= pathRadius*pathRadius) 
+                if (x * x + y * y <= pathRadius * pathRadius)
                 {
                     int drawX = c.tileX + x;
                     int drawY = c.tileY + y;
-                    if (IsInMapRange(drawX, drawY)) 
+                    if (IsInMapRange(drawX, drawY))
                     {
-                        map[drawX,drawY] = val;
+                        map[drawX, drawY] = val;
                     }
                 }
             }
         }
     }
 
-    void OnDrawGizmos() 
+    void OnDrawGizmos()
     {
-        if (map != null) 
+        if (map != null)
         {
-            for (int x = 0; x < width; x ++) 
+            for (int x = 0; x < width; x++)
             {
-                for (int y = 0; y < height; y ++) 
+                for (int y = 0; y < height; y++)
                 {
-                    Gizmos.color = (map[x,y] == 1)? Color.blue:Color.green;
-                    Vector3 pos = new Vector3((-width/2 + x) * unit + transform.position.x,0, (-height/2 + y) * unit + transform.position.z);
-                    Gizmos.DrawCube(pos,new Vector3(unit, 0, unit));
+                    Gizmos.color = (map[x, y] == 1) ? Color.blue : Color.green;
+                    Vector3 pos = new Vector3((-width / 2 + x) * unit + transform.position.x, 0, (-height / 2 + y) * unit + transform.position.z);
+                    Gizmos.DrawCube(pos, new Vector3(unit, 0, unit));
                 }
             }
         }
@@ -379,15 +380,15 @@ public class MapManager : MonoBehaviour
 
     public void FillMap(int val)
     {
-        for (int x = 0; x < width; x += 1) 
+        for (int x = 0; x < width; x += 1)
         {
-            for (int y = 0; y < height; y += 1) 
+            for (int y = 0; y < height; y += 1)
             {
                 map[x, y] = val;
             }
         }
     }
-    
+
     // public void GatherRegion()
     // {
     //     
--- Assets/Game/Scripts/MapGeneration/MapTriggers/NextMapCollider.cs
+++ Assets/Game/Scripts/MapGeneration/MapTriggers/NextMapCollider.cs
@@ -5,12 +5,13 @@ public class NextMapCollider : MonoBehaviour
     private MapManager _mapManager;
     [SerializeField] private GameObject map;
     private Vector3 location;
-    
+
     // Start is called before the first frame update
     private void Start()
     {
         _mapManager = GetComponentInParent<MapManager>();
-        location = _mapManager.transform.position + new Vector3(_mapManager.GetSize().x, 0, 0);
+        location = _mapManager.transform.position + new Vector3(75, 0, 0);
+        // location = _mapManager.transform.position + new Vector3(_mapManager.GetSize().x, 0, 0);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -18,22 +19,25 @@ public class NextMapCollider : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Destroy(gameObject);
-            BuildNextMap();
+            _mapManager.transform.position = location;
+            _mapManager.GenerateMap();
         }
     }
 
-    private void BuildNextMap()
-    {
-        if (_mapManager.mapNumber >= 3)
+    /* 
+        private void BuildNextMap()
         {
-            return;
+            if (_mapManager.mapNumber >= 3)
+            {
+                return;
+            }
+            GameObject nextMap = Instantiate(
+                map,
+                location,
+                Quaternion.identity,
+                transform.parent.parent
+            );
+            nextMap.GetComponent<MapManager>().SetPreviousMap(_mapManager);
         }
-        GameObject nextMap = Instantiate(
-            map, 
-            location, 
-            Quaternion.identity, 
-            transform.parent.parent
-        );
-        nextMap.GetComponent<MapManager>().SetPreviousMap(_mapManager);
-    }
+         */
 }
