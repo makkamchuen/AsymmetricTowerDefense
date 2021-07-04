45f2a54f3ac487cd18e75d385e909798964c326a
Olivia Poon <ysopoon@gmail.com>
2021-07-04 13:44:36 -0700

    - pause sreen is full screen - can change keybind on pause screen - use PauseControl.cs to pause instead of PopOptionMane.cs - updated SpawnByShortcut.cs so that user cannot spawn when game is paused

---
+++ Assets/Game/Scripts/PauseControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseControl : MonoBehaviour
+{
+    public static bool gameIsPaused; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameIsPaused = !gameIsPaused;
+            PauseGame(); 
+        }
+    }
+    void PauseGame()
+    {
+        if (gameIsPaused)
+        {
+            Time.timeScale = 0f; 
+        }
+        else
+        {
+            Time.timeScale = 1; 
+        }
+    }
+
+    public void resumeGame()
+    {
+        gameIsPaused = false;
+        PauseGame(); 
+    }
+}
